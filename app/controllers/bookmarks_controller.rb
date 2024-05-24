c9f4bfd34c135d34a564a742e19b43c2187d2836
Nicolas <nicolas.lisardipro@gmail.com>
2024-05-24 16:03:46 +0200

    Watch List Design List

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,5 +4,21 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
   end
 
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.new(bookmarks_params)
+    @bookmark.movie = @movie
+    @bookmark.list = @list
+    @bookmark.save!
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+
 
 end
